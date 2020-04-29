1e936e4fd91a6acd4d43f72fa1b05312cba4c987
Angelo Nelson <angelonelson@outlook.com>
2020-04-28 20:34:46 -0500

    Fixed formatting and increased text size for nav

--- app.py
+++ app.py
@@ -4,35 +4,50 @@ import config as cfg
 import random
 import string
 from waitress import serve
-conn = psycopg2.connect(user =cfg.info["user"],password = cfg.info["passwd"],host = cfg.info["host"],port = "5432",database = cfg.info["db"])
+conn = psycopg2.connect(user=cfg.info["user"], password=cfg.info["passwd"],
+                        host=cfg.info["host"], port="5432", database=cfg.info["db"])
 cursor = conn.cursor()
 
-app= Flask(__name__)
+app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/login")
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    print(request.method)
+    error = request.args.get('uErr')
+    return render_template("login.html", uErr=error)
+
+
 @app.route("/login/<typeOfService>")
 def loginTyped(typeOfService):
-    return render_template("login.html", typeOfService = typeOfService)
+    return render_template("login.html", typeOfService=typeOfService)
+
+
 @app.route("/register")
 def register():
     return render_template("register.html", fn=True)
+
+
 @app.route("/register2", methods=['post'])
 def register2():
     fname = request.form['fname']
     lname = request.form['lname']
     print(fname)
-    return render_template("register2.html",fname = fname, lname = lname)
+    return render_template("register2.html", fname=fname, lname=lname)
+
+
 @app.route("/register3", methods=['post'])
 def register3():
     fname = request.form['fname']
     lname = request.form['lname']
     uname = request.form['uname']
     pword = request.form['pword']
-    return render_template("register3.html", fname = fname, lname = lname, uname = uname, pword = pword)
+    return render_template("register3.html", fname=fname, lname=lname, uname=uname, pword=pword)
+
+
 @app.route("/registerComplete", methods=['post'])
 def registerC():
     fname = request.form['fname']
@@ -44,41 +59,52 @@ def registerC():
     address = request.form['address']
     city = request.form['city']
     state = request.form['state']
-    insert = "INSERT INTO users VALUES ('"+str(username)+"','"+str(password)+"','"+str(email)+"','"+str(phonenumber)+"','"+str(address)+"','"+str(city)+"','"+str(state)+ "','"+str(fname)+"','"+str(lname)+"')"
+    insert = "INSERT INTO users VALUES ('"+str(username)+"','"+str(password)+"','"+str(email)+"','"+str(
+        phonenumber)+"','"+str(address)+"','"+str(city)+"','"+str(state) + "','"+str(fname)+"','"+str(lname)+"')"
     cursor.execute(insert)
     conn.commit()
     return render_template('registered.html')
+
+
 @app.route("/useraccount", methods=['post'])
 def useraccount():
     username = request.form['uname'].lower()
     password = request.form['pwd']
     typeOfService = request.form['typeOfService']
-    selecting = "SELECT * FROM users WHERE username = " + "'"+str(username)+"' AND password = '"+str(password)+"'"
+    selecting = "SELECT * FROM users WHERE username = " + "'" + \
+        str(username)+"' AND password = '"+str(password)+"'"
     cursor.execute(selecting)
     records = cursor.fetchall()
     if username == 'admin' and len(records) != 0:
         selecting12 = "select * from bookings natural join bookingtimes join users on users.username = bookings.stuname"
         cursor.execute(selecting12)
         records12 = cursor.fetchall()
-        return render_template('adminpage.html', uname = records[0][7], records = records12, len = len(records12), len2 = len(records12[0]))
+        return render_template('adminpage.html', uname=records[0][7], records=records12, len=len(records12), len2=len(records12[0]))
     elif len(records) != 0:
-        return render_template('useraccount.html', uname = records[0][0], fTime = True)
+        return render_template('useraccount.html', uname=records[0][0], fTime=True)
     else:
-        return redirect(url_for('login'))
-@app.route("/admino", methods = ['post'])
+        return redirect(url_for('login', uErr=True))
+
+
+@app.route("/admino", methods=['post'])
 def admin():
-     FN = request.form['FN']
-     LN = request.form['LN']
-     TOT = request.form['TOT']
-     TID = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
-     insert1 = "INSERT INTO teacher VALUES ('"+str(TID)+"','"+str(FN)+"','"+str(LN)+"','"+str(TOT)+"')"
-     cursor.execute(insert1)
-     conn.commit()
-     return render_template('adminpage.html')
-@app.route("/CheckBooking", methods = ['post'])
+    FN = request.form['FN']
+    LN = request.form['LN']
+    TOT = request.form['TOT']
+    TID = ''.join([random.choice(string.ascii_letters + string.digits)
+                   for n in range(6)])
+    insert1 = "INSERT INTO teacher VALUES ('"+str(TID) + \
+        "','"+str(FN)+"','"+str(LN)+"','"+str(TOT)+"')"
+    cursor.execute(insert1)
+    conn.commit()
+    return render_template('adminpage.html')
+
+
+@app.route("/CheckBooking", methods=['post'])
 def checkbooking():
     print(request.form['bid'])
-    selecting = "SELECT * FROM bookings natural join bookingtimes WHERE bid = " + "'"+str(request.form['bid'])+"'"
+    selecting = "SELECT * FROM bookings natural join bookingtimes WHERE bid = " + \
+        "'"+str(request.form['bid'])+"'"
     cursor.execute(selecting)
     records = cursor.fetchall()
     for i in records:
@@ -93,20 +119,26 @@ def checkbooking():
         endSQL = records[0][7]
         tob = records[0][3]
         if teacherSQL:
-            return render_template('useraccount.html',ftime = False, isAcedemic = True, hasMeeting = True, meetingID = meetingIDSQL, teacher = teacherSQL,month = monthSQL, start = startSQL, end = endSQL, day = daySQL, TOB = tob)
+            return render_template('useraccount.html', ftime=False, isAcedemic=True, hasMeeting=True, meetingID=meetingIDSQL, teacher=teacherSQL, month=monthSQL, start=startSQL, end=endSQL, day=daySQL, TOB=tob)
         else:
-            return render_template('useraccount.html',ftime = False, isAcedemic = False, hasMeeting = True, meetingID = meetingIDSQL, teacher = teacherSQL,month = monthSQL, start = startSQL, end = endSQL, day = daySQL, TOB = tob)
+            return render_template('useraccount.html', ftime=False, isAcedemic=False, hasMeeting=True, meetingID=meetingIDSQL, teacher=teacherSQL, month=monthSQL, start=startSQL, end=endSQL, day=daySQL, TOB=tob)
     else:
-        return render_template('useraccount.html',ftime = False, hasMeeting = False)
-@app.route("/CreateBooking", methods = ['post'])
+        return render_template('useraccount.html', ftime=False, hasMeeting=False)
+
+
+@app.route("/CreateBooking", methods=['post'])
 def CB():
     name = request.form['uname']
-    return render_template('CB.html', uname = name)
-@app.route("/CreateBooking2", methods = ['post'])
+    return render_template('CB.html', uname=name)
+
+
+@app.route("/CreateBooking2", methods=['post'])
 def CB2():
     name = request.form['uname']
     typeofbooking = request.form['TOB']
-    return render_template('CB2.html', uname = name,TOB = typeofbooking)
+    return render_template('CB2.html', uname=name, TOB=typeofbooking)
+
+
 @app.route("/CreateBooking3", methods=['post'])
 def CB3():
     name = request.form['uname']
@@ -115,15 +147,19 @@ def CB3():
     day = request.form['day']
     print(name, typeofbooking, month, day)
     if typeofbooking == 'Acedemic Tutoring' or typeofbooking == 'Music Lessons' or typeofbooking == 'ACT and SAT Prep' or typeofbooking == 'Sports Coaching':
-        teachSelect = " SELECT firstname,lastname from teacher join teacherbook on teacher.tid = teacherbook.tid WHERE day = '" +str(day)+"' AND month = '"+ str(month)+"' AND typeofteacher = '" + str(typeofbooking)+"'"
+        teachSelect = " SELECT firstname,lastname from teacher join teacherbook on teacher.tid = teacherbook.tid WHERE day = '" + \
+            str(day)+"' AND month = '" + str(month) + \
+            "' AND typeofteacher = '" + str(typeofbooking)+"'"
         cursor.execute(teachSelect)
         teacherList = cursor.fetchall()
         if len(teacherList) == 0:
-            return render_template('CB2.html', noTeacher = True, uname = name, month = month, day = day, TOB = typeofbooking)
+            return render_template('CB2.html', noTeacher=True, uname=name, month=month, day=day, TOB=typeofbooking)
         newTeacherList = list(dict.fromkeys(teacherList))
-        return render_template('CB3.html', nameList = newTeacherList, uname = name, month = month, day = day,TOB = typeofbooking)
+        return render_template('CB3.html', nameList=newTeacherList, uname=name, month=month, day=day, TOB=typeofbooking)
     else:
-        return render_template('CB3-NAC.html', uname = name, month = month, day = day, TOB = typeofbooking)
+        return render_template('CB3-NAC.html', uname=name, month=month, day=day, TOB=typeofbooking)
+
+
 @app.route("/CreateBooking4", methods=['post'])
 def CB5():
     name = request.form['uname']
@@ -131,10 +167,13 @@ def CB5():
     month = request.form['month']
     day = request.form['day']
     teacher = request.form['teachers']
-    timeSelect = " SELECT starttime,endtime from teacher join teacherbook on teacher.tid = teacherbook.tid WHERE firstname = '"+str(teacher)+"'"
+    timeSelect = " SELECT starttime,endtime from teacher join teacherbook on teacher.tid = teacherbook.tid WHERE firstname = '" + \
+        str(teacher)+"'"
     cursor.execute(timeSelect)
     times = cursor.fetchall()
-    teachSelect = " SELECT firstname,lastname from teacher join teacherbook on teacher.tid = teacherbook.tid WHERE day = '" +str(day)+"' AND month = '"+ str(month)+"' AND typeofteacher = '" + str(typeofbooking)+"'"
+    teachSelect = " SELECT firstname,lastname from teacher join teacherbook on teacher.tid = teacherbook.tid WHERE day = '" + \
+        str(day)+"' AND month = '" + str(month) + \
+        "' AND typeofteacher = '" + str(typeofbooking)+"'"
     cursor.execute(teachSelect)
     teacherList = cursor.fetchall()
     newTeacherList = list(dict.fromkeys(teacherList))
@@ -236,57 +275,73 @@ def CB5():
                 tempList = list(times[i])
                 tempList[j] = "12am"
                 times[i] = tuple(tempList)
-    return render_template('CB4.html', timesList = times, teacherName = teacher, uname = name, month = month, day = day, TOB = typeofbooking)
-@app.route("/Created",methods = ['post'])
+    return render_template('CB4.html', timesList=times, teacherName=teacher, uname=name, month=month, day=day, TOB=typeofbooking)
+
+
+@app.route("/Created", methods=['post'])
 def BC():
     name = request.form['uname']
     month = request.form['month']
     day = request.form['day']
     typeofbooking = request.form['TOB']
     comments = request.form['comments']
-    BID = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
+    BID = ''.join([random.choice(string.ascii_letters + string.digits)
+                   for n in range(6)])
     print("TOB: " + typeofbooking)
     if typeofbooking == 'Acedemic Tutoring' or typeofbooking == 'Music Lessons' or typeofbooking == 'ACT and SAT Prep' or typeofbooking == 'Sports Coaching':
         print("we innie boys")
         teacher = request.form['teacher']
         times = request.form['times']
         goodtimes = times.split()
-        insert1 = "INSERT INTO bookings VALUES ('"+str(BID)+"','"+str(name)+"','"+str(teacher)+"','"+str(typeofbooking)+"')"
+        insert1 = "INSERT INTO bookings VALUES ('"+str(BID)+"','"+str(
+            name)+"','"+str(teacher)+"','"+str(typeofbooking)+"')"
         cursor.execute(insert1)
         conn.commit()
-        insert2 = "INSERT INTO bookingtimes VALUES ('"+str(BID)+"','"+str(month)+"','"+str(day)+"','"+str(goodtimes[0])+"','"+str(goodtimes[1])+"')"
+        insert2 = "INSERT INTO bookingtimes VALUES ('"+str(BID)+"','"+str(
+            month)+"','"+str(day)+"','"+str(goodtimes[0])+"','"+str(goodtimes[1])+"')"
         cursor.execute(insert2)
         conn.commit()
-        fixedst = goodtimes[0].replace('am','')
-        fixedet = goodtimes[1].replace('am','')
-        fixedst = goodtimes[0].replace('pm','')
-        fixedet = goodtimes[1].replace('pm','')
+        fixedst = goodtimes[0].replace('am', '')
+        fixedet = goodtimes[1].replace('am', '')
+        fixedst = goodtimes[0].replace('pm', '')
+        fixedet = goodtimes[1].replace('pm', '')
 
-        remove = "DELETE FROM teacherbook where starttime = '"+str(fixedst)+"' AND endtime = '"+str(fixedet)+"'"
+        remove = "DELETE FROM teacherbook where starttime = '" + \
+            str(fixedst)+"' AND endtime = '"+str(fixedet)+"'"
         cursor.execute(remove)
         conn.commit()
 
-        print(name,month,day,teacher,times,BID,fixedst,fixedet)
-        return render_template('BookingComplete.html',BID = BID)
+        print(name, month, day, teacher, times, BID, fixedst, fixedet)
+        return render_template('BookingComplete.html', BID=BID)
     else:
         starttime = request.form['st']
-        insert1 = "INSERT INTO bookings VALUES ('"+str(BID)+"','"+str(name)+"','"+''+"','"+str(typeofbooking)+"','" + str(comments)+"')"
+        insert1 = "INSERT INTO bookings VALUES ('"+str(BID)+"','"+str(
+            name)+"','"+''+"','"+str(typeofbooking)+"','" + str(comments)+"')"
         cursor.execute(insert1)
         conn.commit()
-        insert2 = "INSERT INTO bookingtimes VALUES ('"+str(BID)+"','"+str(month)+"','"+str(day)+"','"+str(starttime)+"')"
+        insert2 = "INSERT INTO bookingtimes VALUES ('"+str(
+            BID)+"','"+str(month)+"','"+str(day)+"','"+str(starttime)+"')"
         cursor.execute(insert2)
         conn.commit()
-        return render_template('BookingComplete.html',BID = BID)
+        return render_template('BookingComplete.html', BID=BID)
+
+
 @app.route("/ContactUs")
 def CU():
     return render_template("contact.html")
+
+
 @app.route("/MeetTheTeam")
 def MTT():
     return render_template("MTT.html")
+
+
 @app.route("/WurkerPage")
 def WP():
     return render_template('SSTP.html')
-@app.route("/wurkerHandler", methods = ['post'])
+
+
+@app.route("/wurkerHandler", methods=['post'])
 def create():
     tid = request.form['tid']
     month = request.form['month']
@@ -298,31 +353,36 @@ def create():
     records = cursor.fetchall()
     timeelapsed = 0
     if(len(records) == 0):
-        return render_template('SSTP.html', error = True)
+        return render_template('SSTP.html', error=True)
     else:
-         intst = int(st)
-         intet = int(et)
-         print("start: " + str(intst) + "end: " + str(intet))
-         if intst > intet:
+        intst = int(st)
+        intet = int(et)
+        print("start: " + str(intst) + "end: " + str(intet))
+        if intst > intet:
             while intst < 24:
-                insert1 = "INSERT INTO teacherbook VALUES ('"+str(tid)+"','"+str(month)+"','"+str(day)+"','"+str(intst)+"','"+str(intst+1)+"')"
+                insert1 = "INSERT INTO teacherbook VALUES ('"+str(tid)+"','"+str(
+                    month)+"','"+str(day)+"','"+str(intst)+"','"+str(intst+1)+"')"
                 cursor.execute(insert1)
-                intst+=1
+                intst += 1
                 conn.commit()
-            intst = 1;
+            intst = 1
             for i in range(intet - 1):
-                insert1 = "INSERT INTO teacherbook VALUES ('"+str(tid)+"','"+str(month)+"','"+str(day)+"','"+str(intst)+"','"+str(intst+1)+"')"
+                insert1 = "INSERT INTO teacherbook VALUES ('"+str(tid)+"','"+str(
+                    month)+"','"+str(day)+"','"+str(intst)+"','"+str(intst+1)+"')"
                 cursor.execute(insert1)
-                intst+=1
+                intst += 1
                 conn.commit()
-         else:
+        else:
             timeelapsed = intet - intst
             for i in range(timeelapsed):
-                insert1 = "INSERT INTO teacherbook VALUES ('"+str(tid)+"','"+str(month)+"','"+str(day)+"','"+str(intst)+"','"+str(intst+1)+"')"
+                insert1 = "INSERT INTO teacherbook VALUES ('"+str(tid)+"','"+str(
+                    month)+"','"+str(day)+"','"+str(intst)+"','"+str(intst+1)+"')"
                 cursor.execute(insert1)
-                intst+=1
+                intst += 1
                 conn.commit()
-         return render_template('SSTP.html', created = True)
+        return render_template('SSTP.html', created=True)
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host = "0.0.0.0" ,port="69")
+    app.run(debug=True, host="0.0.0.0", port="69")
     #serve(app, listen ='*:80')
