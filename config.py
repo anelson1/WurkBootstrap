7c2930ca426ef3c559fe5d182a6a463aa28d9e91
Angelo Nelson <angelonelson@outlook.com>
2020-05-12 21:27:47 -0500

    Changed email to outlook

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'PissUponYourChimneyKneeGrowBeOnThatBeheminiey'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp-mail.outlook.com.'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
