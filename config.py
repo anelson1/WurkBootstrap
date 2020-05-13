565a65a3095fd29dfbb12a03663fdea17e8ff2bc
Angelo Nelson <angelonelson@outlook.com>
2020-05-12 21:15:54 -0500

    config update again

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ class Config(object):
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get()
-    MAIL_PASSWORD = 'Wowreally123@'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
